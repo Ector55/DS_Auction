0c38ed030577784e5b355415432ac05ab6b6c7ae
ItsMegan605 <meganmaremmani13@gmail.com>
2026-01-12 18:06:42 +0100

    erlang scheleton

--- DistributedProject/ErlangServer/DS_auction_supervisor.erl
+++ DistributedProject/ErlangServer/DS_auction_supervisor.erl
@@ -2,12 +2,25 @@
 %%% @author crazy
 %%% @copyright (C) 2026, <COMPANY>
 %%% @doc
-%%%
+%%% it will start the auctions, it's the auction's parent,
+%% it will generate the aucitons, need to add the crash handling
 %%% @end
 %%% Created : 02. gen 2026 17:04
 %%%-------------------------------------------------------------------
 -module('DS_auction_supervisor').
 -author("crazy").
+-behaviour(supervisor).
 
-%% API
--export([]).
+-export([start_link/0, start_auction/2, init/1]).
+
+%% Starts the supervisor process decide if local or global
+start_link() ->
+  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%spawn delle auction dynamic, to add the specific parameters we want
+%i used the ones from others
+start_auction(AuctionID, EndTime, StartPrice, SellerID) ->
+  supervisor:start_child(?MODULE, [AuctionID, EndTime, StartPrice, SellerID]).
+
+
+%init([]) -> %to be defines i think this is the backup if auctions fail
